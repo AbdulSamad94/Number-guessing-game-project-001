c952c9959b9914b4bfa42c3fb4b5cbb05183d15f
Abdul Samad Siddiqui <160336057+AbdulSamad94@users.noreply.github.com>
2024-03-20 20:54:32 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+import inquirer from "inquirer";
+
+let target = Math.floor(Math.random()*3 + 1);
+
+let message= await inquirer.prompt([{
+    message : "Select any one of the number",
+    type : "list",
+    name : "Num1",
+    choices : ["1", "2", "3"]
+}])
+
+if (message.Num1== target){
+    console.log("You are Correct")
+}
+
+else{
+    console.log("Please! try again")
+}
